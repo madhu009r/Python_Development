68829b57ac5e6f72537877a7384febe0b032a2e3
madhu009r <165657391+madhu009r@users.noreply.github.com>
2025-02-18 21:35:10 +0530

    Add files via upload

---
+++ Python_develop/Dy1.py
@@ -0,0 +1,20 @@
+"""python Class
+
+---Printing Hello--- """
+
+print("Hello world !")
+
+# Data types in python
+# int- Integer-- +infinity to - infinity(-2,-1,0,1,2)
+#float - decimal- 0.1,0.01....-0.1
+#str - cahracter- "a","adds the letters",'s'.
+virat=50
+rohit=89
+# print(virat+rohit)
+
+# multi line variable assignment example
+x,y,z=12,34,5
+print(x,y,z)
+
+d=x+y+z
+print(d)
