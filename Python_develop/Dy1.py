67073caadc07ad3a73537d12d3e17b80be9ff19c
madhu009r <165657391+madhu009r@users.noreply.github.com>
2025-02-18 21:52:08 +0530

    Update Dy1.py

--- Python_develop/Dy1.py
+++ Python_develop/Dy1.py
@@ -18,3 +18,16 @@ print(x,y,z)
 
 d=x+y+z
 print(d)
+# Data types
+# int
+num1=8
+print(type(num1))
+#string
+strin="abb"
+print(type(strin))
+#print(num1+string) will get not possible
+#float
+num2=23.099
+print(type(num2))
+#attempt to add int and float
+print(num1+num2)
