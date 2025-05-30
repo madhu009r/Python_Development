87b08880d4f2db729eacc0e2b3f2fad262c65f0b
madhu009r <165657391+madhu009r@users.noreply.github.com>
2025-05-30 21:29:41 +0530

    Add files via upload

---
+++ Python_develop/Dy1_datatypes.py
@@ -0,0 +1,19 @@
+# Data types
+# int
+num1=8
+print(type(num1))
+
+#string
+strin="abb"
+print(type(strin))
+
+#print(num1+string) will get not possible
+#float
+
+num2=23.099
+print(type(num2))
+#attempt to add int and float
+num3=(num1+num2)
+print(type(num3))
+
+#
